f49d72fafc8e42de33fa3feeed9c027d01dbc468
Titus Wormer <tituswormer@gmail.com>
2018-09-30 13:04:20 +0200

    add webp support

--- src/index.js
+++ src/index.js
@@ -6,15 +6,23 @@ setTimeout(init, 4)
 doc.querySelectorAll('[data-background-src]').forEach(load)
 
 function load(node) {
-  var src = node.dataset.backgroundSrc
+  var defaultSrc = node.dataset.backgroundSrc
+  var src = defaultSrc.replace(/\.(jpg|png)/, '.webp')
   var bg = doc.createElement('div')
   var img = doc.createElement('img')
 
   bg.className = 'lazy'
 
   img.addEventListener('load', onload)
+  img.addEventListener('error', onerror)
   img.src = src
   node.insertBefore(bg, node.firstChild)
+
+  function onerror() {
+    src = defaultSrc
+    img.addEventListener('error', onerror)
+    img.src = src
+  }
 
   function onload() {
     bg.style.backgroundImage = 'url(' + src + ')'
